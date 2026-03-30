a93ac226038189b4b89e7d839999c82cddc8cdae
DeviHS <henrik.sipila@gmail.com>
2026-03-30 22:15:57 +0300

    Forgot password korjailua

---
+++ frontend/script.js
@@ -0,0 +1,5 @@
+"use strict";
+const forgot = document.getElementById("forgot");
+forgot.addEventListener("click", () => {
+    alert("testi");
+});
---
+++ frontend/script.ts
@@ -0,0 +1,6 @@
+const forgot = document.getElementById("forgot") as HTMLButtonElement;
+
+forgot.addEventListener("click", () =>
+{
+alert("testi")
+});
